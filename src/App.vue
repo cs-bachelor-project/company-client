170d71557399c4f031cb610f98688ede5c0f9109
unknown <ullhaq@hotmail.com>
2020-04-27 15:21:39 +0200

    navbar container added

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,35 @@
     </div>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Fixed navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link :to="{name: 'index'}" class="nav-link">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{name: 'users'}" class="nav-link">Users</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{name: 'tasks'}" class="nav-link">Tasks</router-link>
-          </li>
-        </ul>
-        <ul class="navbar-nav float-right">
-          <li class="nav-item" v-if="!isLoggedIn()">
-            <router-link :to="{name: 'login'}" class="nav-link">Login</router-link>
-          </li>
-          <li class="nav-item" v-if="isLoggedIn()">
-            <router-link :to="{name: 'settings'}" class="nav-link">Settings</router-link>
-          </li>
-          <li class="nav-item" v-if="isLoggedIn()">
-            <a href="" class="nav-link" @click.prevent="logout">Logout</a>
-          </li>
-        </ul>
+      <div class="container">
+        <a class="navbar-brand" href="#">Fixed navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link :to="{name: 'index'}" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name: 'users'}" class="nav-link">Users</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name: 'tasks'}" class="nav-link">Tasks</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav float-right">
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <router-link :to="{name: 'login'}" class="nav-link">Login</router-link>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <router-link :to="{name: 'settings'}" class="nav-link">Settings</router-link>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <a href="" class="nav-link" @click.prevent="logout">Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
 
