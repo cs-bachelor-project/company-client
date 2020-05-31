a3c34df9e660da4a7fc353905b00687bcf2a9e64
unknown <ullhaq@hotmail.com>
2020-05-31 22:07:44 +0200

    Your Driver

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <router-link :to="{name: 'index'}" class="navbar-brand">Bachelor Project</router-link>
+        <router-link :to="{name: 'index'}" class="navbar-brand">Your Driver</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
